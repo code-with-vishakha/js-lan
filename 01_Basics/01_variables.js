c0ebe0d2447faa974a7566335996a1ba6474f6e5
code-with-vishakha <vishakhamishra9212@gmail.com>
2025-06-19 14:10:46 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=121121
+var accountEmail="vishakha@gmail.com"
+let accountPassword="232222"
+accountcity="surat"
+
+// accountId=232233
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountcity])
+   
