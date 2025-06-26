a08d82ffde33440337e98907f91431ba601d859a
code-with-vishakha <vishakhamishra9212@gmail.com>
2025-06-26 04:53:47 +0000

    datatypes Done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict;"//treat all js code as newer version
+
+// alert(3+3)//we are using node.js ,not browser
+
+let name="vishakha"
+let age=23
+
+console.log(typeof age)
+console.log(typeof "vishakha")
+
+// datatypes in js
+//  Number
+//  BigInt ==> use for big int values
+//  String ==> ""
+//  Boolean==> true/false
+//  undefined==> value not assign, and its type is undefined
+//  null==> value is empty , its tpe is object
+//  Symbol==>unique
