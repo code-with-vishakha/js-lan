aeec7fe01b954cab5f427d9ff3d243381ce684ce
code-with-vishakha <vishakhamishra9212@gmail.com>
2025-06-26 05:33:48 +0000

    Conversion of Datatypes

---
+++ 01_Basics/03_data_conversion.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//  "33" ==> 33
+//  "33abc" ==> NaN ( not a number)
+// true==>1; false==>0
+
+let myAccount= 1
+
+let booleanmyAccount= Boolean(myAccount)
+// console.log(typeof booleanmyAccount)
+console.log(booleanmyAccount);
+
+// 1==>true; 0==>false;
+// ""==> false
+// "vishakha"==>true
+
+let someNo= 22
+
+let stringno= String(someNo)
+console.log(stringno);
+console.log(typeof stringno)
+
+// 22==>stringno (type)
+// 22==>22 (value)
